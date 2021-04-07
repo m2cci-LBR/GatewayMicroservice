08fcab2427ded55cb7644e39040684944a680c7f
m2cci-LBR <mrlarbi112@gmail.com>
2021-04-07 12:36:37 +0200

    add gateway microservice

---
+++ src/test/java/com/capgemini/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
